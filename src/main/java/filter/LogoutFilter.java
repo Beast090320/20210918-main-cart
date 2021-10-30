bb0de0a5ff09b73323aab606fa61ee104f655a31
MB-study <hunterhope@outlook.com>
2021-10-30 12:26:28 +0800

    main

--- src/main/java/filter/LogoutFilter.java
+++ src/main/java/filter/LogoutFilter.java
@@ -1,9 +1,13 @@
 package filter;
 
 import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import javax.enterprise.context.spi.Context;
 import javax.servlet.FilterChain;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
@@ -20,7 +24,19 @@ public class LogoutFilter extends HttpFilter {
 	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		HttpSession session = req.getSession(false);
-		session.invalidate();
+		ServletContext context = getServletContext();
+		
+		// SSO 移除此帳號
+		if(session.getAttribute("user") != null && context.getAttribute("users") != null) {
+			User user = (User)session.getAttribute("user");
+			Set<User> users = (LinkedHashSet)context.getAttribute("users");
+			System.out.println("SSO users 移除前: " + users);
+			users.remove(user); // 記得在 User 物件要加入 equals() 與 hashCode()
+			System.out.println("SSO users 移除後: " + users);
+			context.setAttribute(getFilterName(), users); // 回存
+		}
+		
+		
 		//RequestDispatcher rd = req.getRequestDispatcher("/form/login.jsp");
 		//rd.forward(req, res);
 		res.sendRedirect(getServletContext().getContextPath() + "/servlet/cart");
